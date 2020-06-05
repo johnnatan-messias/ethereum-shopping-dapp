ab2bfc0994491389313289145dd7024b18a91b20
Johnnatan Messias <johnme@mpi-sws.org>
2020-06-05 13:45:10 +0200

    Now new bought products are inserted into the owned products list automatically

--- src/js/app2.js
+++ src/js/app2.js
@@ -166,7 +166,18 @@ $(document).on('click', '#buy-product', function () {
                         alert("Transaction ID: " + txid)
                         waitForReceipt(txid, function (receipt) {
                             console.log('Receipt', receipt);
-                            alert("Transaction confirmed and included in block " + receipt.blockNumber)
+                            alert("Transaction confirmed and included in block " + receipt.blockNumber);
+                            contract_instance.getProductById.call(productId, function (error, product) {
+                                if (error) {
+                                    console.log(productId, error);
+                                }
+                                else {
+                                    console.log('Owned:', 'productId=' + parseInt(product[0]) + '\tcategoryId=' + parseInt(product[1]) + '\tcategoryName=' + product[2] + '\tproductName=' + product[3] + '\tpriceInSBC=' + parseInt(product[4]));
+                                    $(".owned-products-list").append(
+                                        '<div class="col-lg-3 col-md-4 col-sm-6"><div class="owned__item"><div class="owned__item__pic set-bg" data-setbg="img/product/product-' + parseInt(product[0]) + '.jpg" style="background-image: url(&quot;img/product/product-' + parseInt(product[0]) + '.jpg&quot;);"></div><div class="owned__item__text"><h6><a href="#">' + product[3] + '</a></h6><h5>€ ' + sbc_to_eur(parseInt(product[4])) + '</h5></div></div></div>'
+                                    )
+                                }
+                            });
                         });
                     }
                 });
